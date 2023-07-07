b9d9cf23a46037616d88e76f45a86921a2085d9f
m3rashid <m3rashid.hussain@gmail.com>
2023-07-07 21:15:37 +0530

    update

--- gatekeeper/utils/permissions.ts
+++ gatekeeper/utils/permissions.ts
@@ -1,26 +1,30 @@
 import { IDbSchemaKey, IPermission } from '../models';
 import { modelNames } from '../models/names';
 
-export const permissionValue = [1, 2, 4, 8, 16] as const;
+export const permissionValue = [1, 2, 4, 8, 16, 32, 64] as const;
 export type PermissionValue = (typeof permissionValue)[number];
 
 export const permissionKeys = [
-  'READ',
+  'READ_SELF',
+  'READ_ALL',
   'CREATE',
-  'UPDATE',
-  'DELETE',
-  'BULK_UPDATE',
+  'UPDATE_SELF',
+  'UPDATE_ALL',
+  'DELETE_SELF',
+  'DELETE_ALL',
 ] as const;
 export type PermissionKey = (typeof permissionKeys)[number];
 
 export type Permission = Record<PermissionKey, PermissionValue>;
 
 export const PERMISSION: Permission = {
-  READ: 1,
-  CREATE: 2,
-  UPDATE: 4,
-  DELETE: 8,
-  BULK_UPDATE: 16,
+  READ_SELF: permissionValue[0],
+  READ_ALL: permissionValue[1],
+  CREATE: permissionValue[2],
+  UPDATE_SELF: permissionValue[3],
+  UPDATE_ALL: permissionValue[4],
+  DELETE_SELF: permissionValue[5],
+  DELETE_ALL: permissionValue[6],
 } as const;
 
 export const hasPermission = (userPermission: number, checkFor: number) => {
@@ -43,13 +47,15 @@ export const convertPermissionToReadable = (permission: IPermission) => {
   }));
 };
 
-export const permissionBuilder = (
-  permission: Partial<IPermission>
-): IPermission => {
-  const defaultPermissionValue = 0;
-
+export const permissionBuilder = ({
+  permission,
+  defaultAccess,
+}: {
+  permission: Partial<IPermission>;
+  defaultAccess: PermissionValue;
+}): IPermission => {
   return Object.keys(modelNames).reduce<IPermission>((acc, curr) => {
     const key = curr as IDbSchemaKey;
-    return { ...acc, [key]: permission[key] || defaultPermissionValue };
+    return { ...acc, [key]: permission[key] || defaultAccess };
   }, {} as IPermission);
 };
